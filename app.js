0c63d2c58239dfc258a20174aa1f505a5211eba7
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-07-28 18:29:10 -0300

    challange completed

--- app.js
+++ app.js
@@ -5,25 +5,20 @@ function macsByRadio(probes) {
 
   let reduceProbesArray = [];
 
-  if(!probes){
+  if (!probes) {
     return reduceProbesArray;
   }
 
   for (let { mac, radio } of probes) {
 
-    if (reduceProbesArray.length === 0) {
-      reduceProbesArray.push({ radio, macs: [mac] })
+    const currentProbe = reduceProbesArray.find(probe => probe.radio === radio);
 
+    if (currentProbe) {
+      currentProbe.macs.push(mac)
     } else {
-
-      const currentProbe = reduceProbesArray.find(probe => probe.radio === radio);
-
-      if (currentProbe) {
-        currentProbe.macs.push(mac)
-      } else {
-        reduceProbesArray.push({ radio, macs: [mac] })
-      }
+      reduceProbesArray.push({ radio, macs: [mac] })
     }
+
   }
   return reduceProbesArray;
 }
